07c826e917018b3fc7c1bbc9f841d6c478088d7e
pablomigueldias <pablo.miguel.dias@gmail.com>
2026-01-28 17:14:32 -0300

    fix: allow all cors origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,6 +8,8 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title='Budget Manager API')
 
+origins = ["*"]
+
 origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
