1ce17e7c8b50d216c3599292f28f1842a64b4ce8
pablomigueldias <pablo.miguel.dias@gmail.com>
2026-01-28 16:47:48 -0300

    chore: configure database for production

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,10 +1,18 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os  
 
-DATABASE_URL = 'sqlite:///./manager.db'
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./sql_app.db")
 
-engine = create_engine(DATABASE_URL, connect_args={'check_same_thread': False})
+if SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
+    SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+connect_args = {"check_same_thread": False} if "sqlite" in SQLALCHEMY_DATABASE_URL else {}
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args=connect_args
+)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
